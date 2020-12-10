f7df0b68a11f183fc3f48192a809799a1b9ab6fc
Dawid Mateusiak <mateusiakdawid@gmail.com>
2020-12-10 23:59:13 +0100

    Modyfikacja modelu Film

--- Python - Django 3.0 po polsku (podstawy)/films/filmyweb/models.py
+++ Python - Django 3.0 po polsku (podstawy)/films/filmyweb/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Film(models.Model):
-    tytul = models.CharField(max_length=64)
+    tytul = models.CharField(max_length=64, blank=False, unique=True)
+    rok = models.PositiveSmallIntegerField(default=2000)
+
+    def __str__(self):
+        return self.tytul + f' ({self.rok})'
