eccb0c26abe17be12b7d54916f9102d3451ebb6b
Dawid Mateusiak <mateusiakdawid@gmail.com>
2020-12-12 19:21:29 +0100

    Models fields, Django admin, urls, views, templates

--- Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
+++ Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def wszystkie_filmy(request):
+    #return HttpResponse("<h1>To jest nasz pierwszy test<h1>")
+    return render(request,"filmy.html")
