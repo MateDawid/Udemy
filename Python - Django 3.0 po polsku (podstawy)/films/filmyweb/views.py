c71619189188af5c88a0ce7252fd4ff84fe77619
Dawid Mateusiak <mateusiakdawid@gmail.com>
2020-12-14 23:33:59 +0100

    Templates, pętla for, if-else, static, media

--- Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
+++ Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Film
 
 def wszystkie_filmy(request):
     #return HttpResponse("<h1>To jest nasz pierwszy test<h1>")
-    return render(request,"filmy.html")
+    wszystkie = Film.objects.all()
+    return render(request,"filmy.html", {'filmy': wszystkie})
