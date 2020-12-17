a5af306fc57abeeff86a4471ae4ec6b119bce8cd
Dawid Mateusiak <mateusiakdawid@gmail.com>
2020-12-17 22:28:48 +0100

    CRUD, login i logout

--- Python - Django 3.0 po polsku (podstawy)/films/films/settings.py
+++ Python - Django 3.0 po polsku (podstawy)/films/films/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'moje_media'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'wszystkie_filmy'
+LOGOUT_REDIRECT_URL = 'login'
--- Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
+++ Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Film
+from .forms import FilmForm
+
 
 def wszystkie_filmy(request):
     #return HttpResponse("<h1>To jest nasz pierwszy test<h1>")
     wszystkie = Film.objects.all()
     return render(request,"filmy.html", {'filmy': wszystkie})
+
+@login_required
+def nowy_film(request):
+    form = FilmForm(request.POST or None, request.FILES or None)
+    
+    if form.is_valid():
+        form.save()
+        return redirect(wszystkie_filmy)
+
+    return render(request, 'film_form.html', {'form':form})
+
+@login_required
+def edytuj_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+    form = FilmForm(request.POST or None, request.FILES or None, instance=film)
+    
+    if form.is_valid():
+        form.save()
+        return redirect(wszystkie_filmy)
+    
+    return render(request, 'film_form.html', {'form':form})
+
+@login_required
+def usun_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+
+    if request.method == "POST":
+        film.delete()
+        return redirect(wszystkie_filmy)
+    
+    return render(request, 'potwierdz.html', {'film':film})
