b00477b6da6fb5f0e8333e656ae0aeb6423e2e21
Dawid Mateusiak <mateusiakdawid@gmail.com>
2020-12-29 16:59:13 +0100

    Dodatkowe informacje w filmach

--- Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
+++ Python - Django 3.0 po polsku (podstawy)/films/filmyweb/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Film
-from .forms import FilmForm
+from .models import Film, DodatkoweInfo
+from .forms import FilmForm,DodatkoweInfoForm
 
 
 def wszystkie_filmy(request):
@@ -12,24 +12,37 @@ def wszystkie_filmy(request):
 
 @login_required
 def nowy_film(request):
-    form = FilmForm(request.POST or None, request.FILES or None)
-    
-    if form.is_valid():
-        form.save()
+    form_film = FilmForm(request.POST or None, request.FILES or None)
+    form_dodatkowe = DodatkoweInfoForm(request.POST or None)
+    if all([form_film.is_valid(), form_dodatkowe.is_valid()]):
+        film = form_film.save(commit=False)
+        dodatkowe = form_dodatkowe.save()
+        film.dodatkowe = dodatkowe
+        film.save()
         return redirect(wszystkie_filmy)
 
-    return render(request, 'film_form.html', {'form':form, 'nowy':True})
+    return render(request, 'film_form.html', {'form':form_film,'form_dodatkowe':form_dodatkowe, 'nowy':True})
 
 @login_required
 def edytuj_film(request, id):
     film = get_object_or_404(Film, pk=id)
+
+    try:
+        dodatkowe = DodatkoweInfo.objects.get(film=film.id)
+    except DodatkoweInfo.DoesNotExist:
+        dodatkowe = None
+
     form = FilmForm(request.POST or None, request.FILES or None, instance=film)
-    
-    if form.is_valid():
-        form.save()
+    form_dodatkowe = DodatkoweInfoForm(request.POST or None, instance=dodatkowe)
+
+    if all([form.is_valid(), form_dodatkowe.is_valid()]):
+        film = form.save(commit=False)
+        dodatkowe = form_dodatkowe.save()
+        film.dodatkowe = dodatkowe
+        film.save()
         return redirect(wszystkie_filmy)
     
-    return render(request, 'film_form.html', {'form':form, 'nowy':False})
+    return render(request, 'film_form.html', {'form':form,'form_dodatkowe':form_dodatkowe, 'nowy':False})
 
 @login_required
 def usun_film(request, id):
